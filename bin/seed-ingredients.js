a1c25a1c6bcbc3c8f5d8defc544e7676c0d43fd2
Paula Balcells <paulabalcells@gmail.com>
2018-08-02 16:34:12 +0200

    new recipes

--- bin/seed-ingredients.js
+++ bin/seed-ingredients.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//mongoose.connect(process.env.MONGODB_URI);
 mongoose.connect('mongodb://localhost/fridgeat');
 
 const Ingredient = require('../models/ingredient');
